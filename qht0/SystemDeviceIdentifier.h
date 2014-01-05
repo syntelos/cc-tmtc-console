90cc1d2da96fd30a04fe990c4964ac4f08828c82
John Pritchard <jdp@syntelos.org>
2014-01-05 01:39:57 -0500

    test QHash + SystemDeviceIdentifier

---
+++ qht0/SystemDeviceIdentifier.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2013 John Pritchard, Syntelos.  All rights reserved.
+ */
+#ifndef _SYSTEM_SystemDeviceIdentifier_H
+#define _SYSTEM_SystemDeviceIdentifier_H
+
+#include <QByteArray>
+#include <QHash>
+#include <QString>
+#include <QStringList>
+#include <QVariant>
+
+/*!
+ * Parse a device identifier string into a prefix and optional an
+ * suffix -- separated by a single colon (':').  A default suffix
+ * value is required by the constructor.
+ */
+class SystemDeviceIdentifier : public QVariant {
+
+    static const int InitMetaTypeId;
+
+    QString prefix;
+    quint16 suffix;
+
+ public:
+    /*!
+     * An invalid identifier may be employed for broadcast semantics
+     * in some cases.
+     */
+    static const SystemDeviceIdentifier* BroadcastIdentifier;
+
+    static QString Cat(const QString&, const quint16);
+    /*!
+     * Construct an invalid identifier
+     */
+    SystemDeviceIdentifier();
+    /*!
+     * Construct an identifier using a default suffix
+     */
+    SystemDeviceIdentifier(const QString&, const quint16);
+    /*!
+     * Construct an identifier
+     */
+    SystemDeviceIdentifier(const QString&);
+    /*!
+     * Construct an identifier
+     */
+    SystemDeviceIdentifier(const QByteArray&);
+    /*!
+     * Copy an identifier
+     */
+    SystemDeviceIdentifier(const SystemDeviceIdentifier&);
+    /*!
+     */
+    ~SystemDeviceIdentifier();
+    /*!
+     * The specifically invalid has no prefix and no suffix, and may
+     * be employed with broadcast semantics.  This test is necessary
+     * to the correctness of the operator methods in this case.
+     */
+    bool isSpecial() const;
+    /*!
+     */
+    bool isValid() const;
+    /*!
+     */
+    bool isNotValid() const;
+    /*!
+     */
+    const QString& getPrefix() const;
+    /*!
+     */
+    quint16 getSuffix() const;
+    /*!
+     */
+    QString toString() const;
+
+    QStringList toStringList() const;
+    /*!
+     */
+    bool operator==(const SystemDeviceIdentifier*) const;
+    bool operator==(const SystemDeviceIdentifier&) const;
+    /*!
+     */
+    bool operator!=(const SystemDeviceIdentifier*) const;
+    bool operator!=(const SystemDeviceIdentifier&) const;
+    /*!
+     */
+    bool operator<(const SystemDeviceIdentifier* that) const;
+    bool operator<(const SystemDeviceIdentifier& that) const;
+    /*!
+     */
+    bool operator<=(const SystemDeviceIdentifier* that) const;
+    bool operator<=(const SystemDeviceIdentifier& that) const;
+    /*!
+     */
+    bool operator>(const SystemDeviceIdentifier* that) const;
+    bool operator>(const SystemDeviceIdentifier& that) const;
+    /*!
+     */
+    bool operator>=(const SystemDeviceIdentifier* that) const;
+    bool operator>=(const SystemDeviceIdentifier& that) const;
+};
+
+uint qHash ( const SystemDeviceIdentifier&);
+
+#endif
---
+++ qht0/qht0.pro
@@ -0,0 +1,13 @@
+TARGET = qht0
+TEMPLATE = app
+
+QT     += network
+
+CONFIG += debug
+#CONFIG += release
+
+SOURCES += Main.cpp \
+           SystemDeviceIdentifier.cpp
+
+HEADERS += SystemDeviceIdentifier.h
+
